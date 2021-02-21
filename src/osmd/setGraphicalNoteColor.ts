b11fe3768b5a560c567227f902514c4a2b5f7d78
abudaan <daniel@abumarkub.net>
2021-02-21 07:50:39 +0100

    remove debug

--- src/osmd/setGraphicalNoteColor.ts
+++ src/osmd/setGraphicalNoteColor.ts
@@ -6,7 +6,7 @@
 export const setGraphicalNoteColor = (el: SVGElement, color: string) => {
   const stems = el.getElementsByClassName("vf-stem");
   const noteheads = el.getElementsByClassName("vf-notehead");
-  console.log(noteheads);
+  // console.log(noteheads);
   for (let i = 0; i < stems.length; i++) {
     const stem = stems[i];
     if (stem.firstChild !== null) {
